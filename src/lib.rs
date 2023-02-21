7f445ce82d4e4a1405e297670bb32122689990a9
sn99 <siddharthn.099@gmail.com>
2023-02-21 10:38:11 +0530

    update examples mad remove main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -14,17 +14,16 @@
 //! Usage:
 //!
 //! ```ignore
+//! use query_wmi::{COMLibrary, Variant, WMIConnection};
 //! use query_wmi::computer_hardware::{
-//!     get_Win32_CDROMDrive, get_Win32_ComputerSystem, get_Win32_OperatingSystem,
+//!     get_Win32_CDROMDrive, get_Win32_ComputerSystem,
 //!     get_Win32_PCMCIAController, get_Win32_PnPEntity, get_Win32_Processor,
 //!     get_Win32_SystemEnclosure, get_Win32_TapeDrive, get_Win32_USBHub,
 //! };
-//!
-//! use wmi::COMLibrary;
+//! use query_wmi::operating_systems::get_Win32_OperatingSystem;
 //!
 //! fn main() -> Result<(), Box<dyn std::error::Error>> {
 //!     let com_con = COMLibrary::new()?;
-//!
 //!     dbg!(get_Win32_OperatingSystem(com_con)?);
 //!     dbg!(get_Win32_CDROMDrive(com_con)?);
 //!     dbg!(get_Win32_ComputerSystem(com_con)?);
@@ -34,7 +33,6 @@
 //!     dbg!(get_Win32_SystemEnclosure(com_con)?);
 //!     dbg!(get_Win32_USBHub(com_con)?);
 //!     dbg!(get_Win32_TapeDrive(com_con)?);
-//!
 //!     Ok(())
 //! }
 //! ```
